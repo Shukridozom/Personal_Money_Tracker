4745e8bc28651077949b64b18545db3dfee2606b
Shukridozom <Shukridozom@hotmail.com>
2023-09-13 21:06:40 +0300

    Configuring connection to MySQL [Database name: Personal_Money_Tracker]

---
+++ PersonalMoneyTracker/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PersonalMoneyTracker
+{
+    public class AppContext : DbContext
+    {
+
+        private readonly IConfiguration _config;
+        public AppContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_config["ConnectionStrings:Url"]);
+        }
+    }
+}
