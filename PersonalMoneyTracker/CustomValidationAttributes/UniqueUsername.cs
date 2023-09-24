4e1e711f210bb051cf6ce5032f4dc58b429597ed
Shukridozom <Shukridozom@hotmail.com>
2023-09-25 02:38:29 +0300

    Adding a custom validation attribute: UniqueUsername

---
+++ PersonalMoneyTracker/CustomValidationAttributes/UniqueUsername.cs
@@ -0,0 +1,24 @@
+﻿using PersonalMoneyTracker.Core.Models;
+using PersonalMoneyTracker.Core.Repositories;
+using PersonalMoneyTracker.Persistence;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalMoneyTracker.CustomValidationAttributes
+{
+    public class UniqueUsername : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            User username;
+            using (var context = new AppDbContext())
+            {
+                username = context.Users.SingleOrDefault(u => u.Username == (string)validationContext.ObjectInstance);
+            }
+
+            if (username == null)
+                return ValidationResult.Success;
+
+            return new ValidationResult("Username already exists");
+        }
+    }
+}
