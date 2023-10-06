a958910e3b93cafbeff04b63a9383e52d8919244
Shukridozom <Shukridozom@hotmail.com>
2023-10-06 19:19:08 +0300

    Adding Transactions API Controller

---
+++ PersonalMoneyTracker/Controllers/TransactionsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PersonalMoneyTracker.Core.Repositories;
+using System.Security.Claims;
+
+namespace PersonalMoneyTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        public TransactionsController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+
+        private int GetLoggedInUserId()
+        {
+            var claim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+            return Int32.Parse(claim.Value);
+        }
+    }
+
+
+}
