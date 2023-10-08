084eb765ea76a04a91c97e3280659d4c5149f9ae
Shukridozom <Shukridozom@hotmail.com>
2023-10-08 05:18:41 +0300

    Fixing a bug: removing TransactionCategoryId column from Transactions table

--- PersonalMoneyTracker/Core/Domains/TransactionType.cs
+++ PersonalMoneyTracker/Core/Domains/TransactionType.cs
@@ -4,13 +4,10 @@
     {
         public TransactionType()
         {
-            Transactions = new List<Transaction>();
             TransactionCategories = new List<TransactionCategory>();
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public IList<TransactionCategory> TransactionCategories { get; set; }
-        public IList<Transaction> Transactions { get; set; }
-
     }
 }
--- PersonalMoneyTracker/Migrations/20231008014124_DropTransactionTypeColumnFromTransactionsTable.Designer.cs
+++ PersonalMoneyTracker/Migrations/20231008014124_DropTransactionTypeColumnFromTransactionsTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonalMoneyTracker.Persistence;
 
@@ -10,9 +11,11 @@ using PersonalMoneyTracker.Persistence;
 namespace PersonalMoneyTracker.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20231008014124_DropTransactionTypeColumnFromTransactionsTable")]
+    partial class DropTransactionTypeColumnFromTransactionsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
